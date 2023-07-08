f27d55d65c1f58beae34cf109e2c996b7d1dd8de
JoFel7 <josuefeliciano7@gmail.com>
2023-07-08 00:05:43 -0400

    continued working on CSS and started work on JS

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+// Document query selectors
+var startBtn = document.querySelector('#start-button');
+var timerEl = document.querySelector('#timer');
+var mainEl = document.querySelector('#main');
+var homeLi = document.querySelector('#home-link');
+var highscoreLi = document.querySelector('#highscore-link')
+
+//Variables
+var timerInterval;
+var secondsLeft;
+var quizSelection;
+var quizPrompt;
+var quizAnswers;
+
+//Quiz Prompts and Answers arrays
+const javascriptQuestions = [
+    'Commonly used data types DO NOT include:',
+    'The condition in an if / else statement is enclosed within ______.',
+    'Arrays in JavaScript can be used to store ______.',
+    'String values must be enclosed within ______ when being assigned to variables.',
+    'A very useful tool used during development and debugging for printing content to the debugger is:'
+];
+
+const javascriptAnswers = [
+    [['strings', false], ['booleans', false], ['alerts', true], ['numbers', false]],
+    [['quotes', false], ['curly brackets', false], ['parentheses', true], ['square brackets', false]],
+    [['numbers and strings', false], ['other arrays', false], ['booleans', false], ['all the above', true]],
+    [['commas', false], ['curly brackets', false], ['quotes', true], ['parentheses', false]],
+    [['JavaScript', false], ['terminal/bash', false], ['for loops'], ['console.log', true]]
+];
+
